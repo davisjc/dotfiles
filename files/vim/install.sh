1f2b017278efabecb6b10f36a3fb9a3c2fe5ee5b
Johnathan Davis <davisjc@users.noreply.github.com>
2015-05-20 00:25:08 -0700

    Adding initial Vundle boostrapping.

--- files/vim/install.sh
+++ files/vim/install.sh
@@ -6,5 +6,6 @@ INSTALL_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 $LINK $INSTALL_DIR/vimrc $HOME/.vimrc
 
 mkdir -p $HOME/.vim
-$LINK $INSTALL_DIR/bundle $HOME/.vim/bundle
+$LINK $INSTALL_DIR/bootstrap $HOME/.vim/bootstrap
+
 
