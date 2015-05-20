748e736cdb7e0a0896823c63f6a289519b9a24fa
Johnathan Davis <davisjc@users.noreply.github.com>
2015-05-20 00:04:56 -0700

    Initial installer for vimrc.

---
+++ files/vim/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+LINK=$DOTFILES/core/link.sh
+INSTALL_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+$LINK $INSTALL_DIR/vimrc $HOME/.vimrc
+
+mkdir -p $HOME/.vim
+$LINK $INSTALL_DIR/bundle $HOME/.vim/bundle
+
