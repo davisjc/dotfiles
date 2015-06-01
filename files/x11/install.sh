64370f3350539bfebd4ca275224e35583f78d117
Johnathan Davis <davisjc@users.noreply.github.com>
2015-05-31 22:09:29 -0700

    Adding Xmodmap to x11 settings.

--- files/x11/install.sh
+++ files/x11/install.sh
@@ -6,4 +6,5 @@ INSTALL_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 $LINK $INSTALL_DIR/xinitrc $HOME/.xinitrc
 $LINK $INSTALL_DIR/xsession $HOME/.xsession
 $LINK $INSTALL_DIR/Xresources $HOME/.Xresources
+$LINK $INSTALL_DIR/Xmodmap $HOME/.Xmodmap
 
