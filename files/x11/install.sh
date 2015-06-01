ce2ef3e6f23b7afbd692c4ea91f99b72903286e3
Johnathan Davis <davisjc@users.noreply.github.com>
2015-05-31 23:30:33 -0700

    Adding compton to x11/

--- files/x11/install.sh
+++ files/x11/install.sh
@@ -8,3 +8,6 @@ $LINK $INSTALL_DIR/xsession $HOME/.xsession
 $LINK $INSTALL_DIR/Xresources $HOME/.Xresources
 $LINK $INSTALL_DIR/Xmodmap $HOME/.Xmodmap
 
+mkdir -p $HOME/.config
+$LINK $INSTALL_DIR/compton.conf $HOME/.config/compton.conf
+
