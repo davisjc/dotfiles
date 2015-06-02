a6491c08e58de20df677cf8802da44836edaac43
Johnathan Davis <davisjc@users.noreply.github.com>
2015-06-01 18:23:26 -0700

    Split zshrc into Linux/OSX modules.

--- files/zsh/install.sh
+++ files/zsh/install.sh
@@ -6,3 +6,9 @@ INSTALL_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 $LINK $INSTALL_DIR/zshrc $HOME/.zshrc
 $LINK $INSTALL_DIR/zprofile $HOME/.zprofile
 
+if [[ "$(uname)" == "Linux" ]]; then
+	$LINK $INSTALL_DIR/zshrc.linux $HOME/.zshrc.linux
+fi
+if [[ "$(uname)" == "Darwin" ]]; then
+	$LINK $INSTALL_DIR/zshrc.osx $HOME/.zshrc.osx
+fi
