a91728701c282737705530eed83aed0e8995a824
Johnathan Davis <davisjc@users.noreply.github.com>
2015-06-09 23:58:41 -0700

    Separated linux mpd settings.

--- files/mpd/install.sh
+++ files/mpd/install.sh
@@ -4,5 +4,7 @@ LINK=$DOTFILES/core/link.sh
 INSTALL_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 mkdir -p $HOME/.config/mpd
-$LINK $INSTALL_DIR/mpd.conf $HOME/.config/mpd/mpd.conf
+if [[ "$(uname)" == "Linux" ]]; then
+    $LINK $INSTALL_DIR/mpd.conf.linux $HOME/.config/mpd/mpd.conf
+fi
 
