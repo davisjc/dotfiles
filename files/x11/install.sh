d20ec5dd93921bf6819195b55ddedd3a14ea8d60
Johnathan Davis <davisjc@users.noreply.github.com>
2015-06-13 16:16:56 -0700

    * Added xvars * Added local i3 config loading * Added local Xresources config loading

--- files/x11/install.sh
+++ files/x11/install.sh
@@ -7,6 +7,7 @@ fi
 LINK=$DOTFILES/core/link.sh
 INSTALL_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+$LINK $INSTALL_DIR/xvars $HOME/.xvars
 $LINK $INSTALL_DIR/xinitrc $HOME/.xinitrc
 $LINK $INSTALL_DIR/xsession $HOME/.xsession
 $LINK $INSTALL_DIR/Xresources $HOME/.Xresources
