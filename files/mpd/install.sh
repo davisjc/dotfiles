7a967e619afc578f5af29b066897557c48105440
Johnathan Davis <davisjc@users.noreply.github.com>
2016-01-24 16:11:57 -0800

    Create necessary files in mpd install script

--- files/mpd/install.sh
+++ files/mpd/install.sh
@@ -3,8 +3,14 @@
 LINK=$DOTFILES/core/link.sh
 INSTALL_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+mkdir -p $HOME/.config/mpd
+mkdir -p $HOME/.config/mpd/playlists
+touch $HOME/.config/mpd/log
+touch $HOME/.config/mpd/pid
+touch $HOME/.config/mpd/state
+touch $HOME/.config/mpd/sticker.sql
+
 if [[ "$(uname)" == "Linux" ]]; then
-    mkdir -p $HOME/.config/mpd
     $LINK $INSTALL_DIR/mpd.conf.linux $HOME/.config/mpd/mpd.conf
 fi
 
