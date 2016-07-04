0e82495e94ac9044d9e45be0a2e5b7ca9657d461
Johnathan Davis <davisjc@users.noreply.github.com>
2016-07-04 13:54:25 -0700

    Fixed typo in mpv config installer

--- files/mpv/install.sh
+++ files/mpv/install.sh
@@ -8,6 +8,6 @@ link=$DOTFILES/link.sh
 install_dir=$(cd "$(dirname "$0")"; pwd -P)
 
 mkdir -p $HOME/.mpv
-$link $instALL_DIR/input.conf $HOME/.mpv/input.conf
-$link $instALL_DIR/config $HOME/.mpv/config
+$link $install_dir/input.conf $HOME/.mpv/input.conf
+$link $install_dir/config $HOME/.mpv/config
 
