a9146527ed42907e11b7e301585b9bf53b17e106
Johnathan Davis <davisjc@users.noreply.github.com>
2017-03-08 21:15:00 +0000

    Update spacemacs config
    Use hbw's aurora theme
    Use hbw's C formatting
    Use C-c as escape

--- files/spacemacs/install.sh
+++ files/spacemacs/install.sh
@@ -4,4 +4,5 @@ link=$DOTFILES/link.sh
 install_dir=$(cd "$(dirname "$0")"; pwd -P)
 
 $link $install_dir/spacemacs $HOME/.spacemacs
+$link $install_dir/themes/vim-aurora.el $HOME/.emacs.d/private/local/vim-aurora.el
 
