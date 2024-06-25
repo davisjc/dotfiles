ffcd0032c539c0fad9514ff9330ad09aa8c64ea5
Johnathan Davis <davisjc@users.noreply.github.com>
2024-06-24 22:41:44 -0700

    ipython: add a few more defaults
    This mainly gets rid off currently bad color rendering and distracting
    auto-completion prompts.

--- files/ipython/ipython_config.py
+++ files/ipython/ipython_config.py
@@ -1,3 +1,6 @@
 c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
 c.TerminalInteractiveShell.prompt_includes_vi_mode = False
+c.TerminalInteractiveShell.colors = 'NoColor'
+c.TerminalInteractiveShell.autosuggestions_provider = None
+c.TerminalIPythonApp.display_banner = False
